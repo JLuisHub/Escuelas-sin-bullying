9cd2db88e9ca9f1aaeab44c936bfa3d980ee94c7
Roberto Tovar <robertotovar290501@gmail.com>
2022-10-22 12:52:48 -0500

    Agrega proyectos empezados
    Es el inicio del desarrollo por cambio de repo

---
+++ aplicacion-web/bullying/app/Http/Controllers/V1/ReporteAPIController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+use App\Models\Reporte;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Exception;
+use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\DB;
+
+
+class ReporteAPIController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $reporte_temp = new Reporte();
+        $reporte_temp->id_docente = $request->id_docente;
+        $reporte_temp->id_estudiante = $request->id_estudiante;
+        $reporte_temp->descripcion = $request->descripcion;
+        $reporte_temp->fecha = $request->fecha;
+
+
+        // Se notificara del reporte 
+        $tutores_legales_encontrados = DB::table('estudiantes_tutores_legales')->where('id_estudiante', $request->id_estudiante)->get();
+        
+        if( empty($tutores_legales_encontrados[0]) ){
+            
+        }
+
+
+        try{
+            $reporte_temp->save();
+            return "Guardado con éxito";
+        }
+        catch(exception $e){
+            return "Hubo un problema al guardar los datos, intente más tarde";
+        }
+
+    }
+
+    public function destroy(Request $request,Reporte $reporte)
+    {
+        $reporte = Reporte::destroy($id_reporte=($request->id_reporte));
+        if($reporte == 0){
+            return "Hubo un error al eliminar el reporte, intente más tarde";
+        }else{
+            return "Eliminado con éxito";
+        }
+    }
+
+}
