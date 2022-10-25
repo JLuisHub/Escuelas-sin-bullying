643af0c89b67ffa9cd493fa1fb3a190077713845
J. Luis Vargas <112353437+JLuisHub@users.noreply.github.com>
2022-10-24 22:13:48 -0500

    Delete app-movil/EscuelaSinBullying directory

--- app-movil/EscuelaSinBullying/db/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', 'root');
-$database = mysqli_select_db($conn, 'esb');
-
-$encodedData = file_get_contents('php://input');  // take data from react native fetch API
-$decodedData = json_decode($encodedData, true);
